4dc824b6f206dedae4e834b8cdb17d3edc471281
Shane Menezes <shane.ryan.menezes@gmail.com>
2024-03-05 15:01:05 +0530

    button

---
+++ public/index.js
@@ -0,0 +1,12 @@
+//grab everyting we need
+
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+
+//add event listeners
+
+btn.addEventListener('click',()=> {
+    menu.classList.toggle('hidden');
+}
+)
